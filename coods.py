2a922f17c2ad33bcb9cebda3cd19c30c3c163b61
Marwamedha <102667454+Marwamedha@users.noreply.github.com>
2022-03-31 21:09:20 +0200

    Update coods.py
    Adding 4 different function.

--- coods.py
+++ coods.py
@@ -1,46 +1,56 @@
-def f(s):
-    t = s[0]
-    for i in range(len(s)-1):
-        if(s[i+1] != s[0]):
-            t += s[i+1]
-        elif(s[i+1:i+len(t)+1] != t):
-            t += s[i+1]
-        else:
-            break
-    k=s.count(t)
- 
-    return (t, k)
-
 def is_palindrome(s):
-    s=s.lower()
-    b=s[::-1]
-    if s==b:
-        return True
-    else:
-        return False
-
-
-def solve (str1, str2): 
-    locs = '' 
-    k=str1.find('*')
-    if str1 == str2:
-        return True
-    elif len(str1) > len(str2)and ('*' in str1):
-        z=str1.replace('*','')
-        print(z)
-        if z == str2:
+    try:
+        s=s.lower()
+        b=s[::-1]
+        if s==b:
             return True
         else:
             return False
-    elif (len(str1) <= len(str2) ) and ('*' in str1):
-        for i in range(k, len(str2)): 
-            locs+=str2[i]
-            c=str1.replace('*',locs)
-            if len(c) == len(str2) and c== str2 :
-                return True
-            elif len(c) == len(str2) and c!= str2 :
-                return False
+    except:
+        print("wrong input.")
+        
+        
+
+def solve (str1, str2):
+    try:
+        if (str1 == str2):
+            return True
+
+        elif("*" in str1):
+            astric_loc = str1.find('*')
+            new_str = str1.replace("*", str2[astric_loc:astric_loc+1+(len(str2)-len(str1))])
+            return new_str == str2
+        else:
+            return False
+    except:
+        print("Wrong input!")
+        
+        
+def f(s):
+    try:
+        t = s[0]
+        for i in range(len(s)-1):
+            if(s[i+1] != s[0]):
+                t += s[i+1]
+            elif(s[i+1:i+len(t)+1] != t):
+                t += s[i+1]
             else:
-                c =''            
-    else :
-        return False
+                break
+        k=s.count(t)
+
+        return (t, k)
+    except:
+        print("wrong input")
+        
+        
+        
+def find_nth_occurrence(substring, string, occurrence=1):
+    try:
+        loc = 0
+        for i in range(occurrence):
+            loc = string.find(substring, loc)
+            loc += 1
+
+        return loc - 1
+    except:
+        print("Wrong input!")

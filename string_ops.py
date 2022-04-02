add8dec341fee3609318e80038e761ff941393a3
Marwamedha <102667454+Marwamedha@users.noreply.github.com>
2022-04-02 16:35:06 +0200

    Update string_ops.py

--- string_ops.py
+++ string_ops.py
@@ -2,4 +2,4 @@ import coods
 coods.is_palindrome('kokok')
 coods.solve('non*','nono')
 coods.f("abcde")
-coods.find_nth_occurrence('example',1)
+coods.find_nth_occurrence('TestTest', 'TestTestTestTest',1)
